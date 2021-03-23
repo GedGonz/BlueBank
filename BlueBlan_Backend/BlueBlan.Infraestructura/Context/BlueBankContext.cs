90289d823d6abf85b1d923ea9211022834568bed
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-23 13:08:50 -0600

    configure fluent api

--- BlueBlan_Backend/BlueBlan.Infraestructura/Context/BlueBankContext.cs
+++ BlueBlan_Backend/BlueBlan.Infraestructura/Context/BlueBankContext.cs
@@ -20,6 +20,39 @@ namespace BlueBlan.Infraestructura.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configure Fluent api
+            modelBuilder.Entity<Account>(entity =>
+            {
+
+                entity.Property(e => e.AccountId).ValueGeneratedOnAdd();
+                entity.HasKey(e => e.AccountId);
+
+                entity.Property(e => e.AccountId).HasColumnName("ID");
+
+                entity.Property(e => e.Value).HasColumnType("DECIMAL(5,2)");
+                entity.Property(e => e.Number).HasColumnType("VARCHAR(15)");
+
+            });
+
+            modelBuilder.Entity<Client>(entity =>
+            {
+
+                entity.Property(e => e.ClientId).ValueGeneratedOnAdd();
+                entity.HasKey(e => e.ClientId);
+
+                entity.Property(e => e.ClientId).HasColumnName("ID");
+
+                entity.Property(e => e.Name).HasColumnType("VARCHAR(30)");
+                entity.Property(e => e.LastName).HasColumnType("VARCHAR(30)");
+
+            });
         }
 
     }
--- BlueBlan_Backend/BlueBlan_Backend/Startup.cs
+++ BlueBlan_Backend/BlueBlan_Backend/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlueBlan.Infraestructura.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,11 @@ namespace BlueBlan_Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
+
+            services.AddDbContext<BlueBankContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("BluBankDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
