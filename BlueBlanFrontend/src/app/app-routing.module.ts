f9dbfb1e17559016b81aad994fb69290e28b83ab
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-24 17:03:09 -0600

    creating routes

--- BlueBlanFrontend/src/app/app-routing.module.ts
+++ BlueBlanFrontend/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from "./components/account/account.component";
+import { ConsultAccountComponent } from "./components/consult-account/consult-account.component";
+import { NewAccountComponent } from "./components/new-account/new-account.component";
+import { MoveAccountComponent } from "./components/move-account/move-account.component";
+import { HistoriMoveAccountComponent } from "./components/histori-move-account/histori-move-account.component";
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'account',component:AccountComponent},
+  {path:'consult',component:ConsultAccountComponent},
+  {path:'new',component:NewAccountComponent},
+  {path:'move',component:MoveAccountComponent},
+  {path:'history',component:HistoriMoveAccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
